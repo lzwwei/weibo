419b709aba44feaab55b9cb0dc14d3b863eda76b
1215334105@qq.com <vagrant@homestead>
2019-08-27 11:06:48 +0000

    登陆成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,10 +7,12 @@
 <body>
    @include('layouts._header')
    <div class='container'>
-       @include('shared._messages')
-       @yield('content')
+       <div class='offset-md-1 col-md-10'>
+           @include('shared._messages')
+           @yield('content')
+       </div>    
    </div>
       @include('layouts._footer')   
-
+   <script src="{{mix('js/app.js')}}"></script>
 </body>
 </html>
