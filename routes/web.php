b8820eb9d761f6b7eeb347d83fd613d29cf32280
1215334105@qq.com <vagrant@homestead>
2019-08-27 12:44:19 +0000

    更新用户资料

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,6 @@ Route::get('/login','SessionController@create')->name('login');
 Route::post('/login','SessionController@store')->name('login');
 
 Route::delete('/logout','SessionController@destroy')->name('logout');
+
+Route::get('/users/{user}/edit', 'UserController@edit')->name('users.edit');
+
