b7ca31f919901d02f195da2861566f01260fda94
1215334105@qq.com <vagrant@homestead>
2019-08-27 07:58:10 +0000

    用户显示界面

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
 	public function create()
 	{
 	    return view('user.create');
+	}
+
+	public function show(User $user)
+	{
+	    return view('user.show', compact('user'));
 	}
 }
--- app/User.php
+++ app/User.php
@@ -6,6 +6,8 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -36,4 +38,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function gravatar($size='100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+	return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+
+
 }
